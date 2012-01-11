cfbb7d214978767f9bab016a5c00f44680126063
David Levine <levinedl@acm.org>
2012-01-11 16:45:53 -0600

    Added #include <grp.h>.  It's needed on Linux, should be harmless on others.

--- uip/slocal.c
+++ uip/slocal.c
@@ -34,6 +34,10 @@
 #include <sys/ioctl.h>
 #include <fcntl.h>
 
+/* Hopefully, grp.h declares initgroups().  If we run into a platform
+   where it doesn't, we could consider declaring it here as well. */
+#include <grp.h>
+
 /* This define is needed for Berkeley db v2 and above to
  * make the header file expose the 'historical' ndbm APIs.
  * We define it unconditionally because this is simple and
