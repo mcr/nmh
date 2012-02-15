23af631076209b31fb28feb90a779ff8dcc153a2
Ken Hornstein <kenh@pobox.com>
2012-02-15 15:54:47 -0500

    Create test cases for the new format functions for local address writing.

---
+++ test/getfullname.c
@@ -0,0 +1,32 @@
+/*
+ * getfullname.c - Extract a user's name out of the GECOS field in
+ *		   the password file.
+ *
+ * This code is Copyright (c) 2012, by the authors of nmh.  See the
+ * COPYRIGHT file in the root directory of the nmh distribution for
+ * complete copyright information.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
+
+int
+main(int argc, char *argv[])
+{
+	struct passwd *pwd;
+
+	pwd = getpwuid(getuid());
+
+	if (! pwd) {
+		fprintf(stderr, "Unable to retrieve user info for "
+			"userid %d\n", getuid());
+		exit(1);
+	}
+
+	printf("%s\n", pwd->pw_gecos);
+
+	exit(0);
+}
