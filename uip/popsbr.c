4eaf25e2a1d634b6f7bfa01a96e6e86768837e9b
David Levine <levinedl@acm.org>
2012-02-16 22:56:02 -0600

    Added a couple of NMH_UNUSED's to suppress warnings when building without Cyrus SASL.

--- uip/popsbr.c
+++ uip/popsbr.c
@@ -418,6 +418,11 @@ int
 pop_init (char *host, char *port, char *user, char *pass, char *proxy,
 	  int snoop, int sasl, char *mech)
 {
+#ifndef CYRUS_SASL
+    NMH_UNUSED (sasl);
+    NMH_UNUSED (mech);
+#endif /* ! CYRUS_SASL */
+
     int fd1, fd2;
     char buffer[BUFSIZ];
 
