7cc0e9952d85f30d1d8ce85cc7a9eb0a5e52e202
David Levine <levinedl@acm.org>
2012-03-16 21:07:52 -0500

    Automated generation of mh-chart.man.

---
+++ man/mh-chart-gen.sh
@@ -0,0 +1,36 @@
+#! /bin/sh
+#
+# Generates mh-chart.man from other .man files that have a
+# SYNOPSIS section.
+
+nmhmandir=`dirname $0`
+
+cat <<'EOF'
+.\"
+.\" %nmhwarning%
+.\"
+.TH MH-CHART %manext1% "%nmhdate%" MH.6.8 [%nmhversion%]
+.SH NAME
+mh-chart \- Chart of nmh Commands
+.SH SYNOPSIS
+.na
+EOF
+
+for i in $nmhmandir/*.man; do
+  if ! echo $i | grep 'mh-chart.man' >/dev/null; then
+    if grep '^\.ad' "$i" >/dev/null; then
+      #### Extract lines from just after .SH SYNOPSIS to just before .ad.
+      #### Filter out the "typical usage:" section in pick.man.
+      awk '/.SH SYNOPSIS/,/^(\.ad|typical usage:)/' "$i" | \
+        grep -Ev '^(\.SH SYNOPSIS|\.na|\.ad|typical usage:)'
+      echo
+    fi
+  fi
+done
+
+cat <<'EOF'
+.ad
+
+.SH "SEE ALSO"
+nmh(1)
+EOF
