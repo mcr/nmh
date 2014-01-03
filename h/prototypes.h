3ded1483f68e20335202b24002cbba2e9ef80c85
Ken Hornstein <kenh@pobox.com>
2014-01-03 09:27:39 -0500

    Remove checks for termcap.h and ncurses/termcap.h, since we're not using those header files anymore.

--- h/prototypes.h
+++ h/prototypes.h
@@ -7,18 +7,6 @@
  */
 
 /*
- * missing system prototypes
- */
-#if ! defined(HAVE_TERMCAP_H) && ! defined (HAVE_NCURSES_TERMCAP_H)
-  extern int tgetent (char *bp, char *name);
-  extern int tgetnum (char *id);
-  extern int tgetflag (char *id);
-  extern char *tgetstr (char *id, char **area);
-  extern char *tgoto (char *cm, int destcol, int destline);
-  extern int tputs (char *cp, int affcnt, int (*outc) (int));
-#endif
-
-/*
  * prototype from config.h
  */
 char *etcpath(char *);
