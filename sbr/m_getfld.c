d2520ac7054ad75d60342606bf13c821305d958c
David Levine <levinedl@acm.org>
2014-03-14 19:04:45 -0500

    Compare character with EOF using signed comparison because EOF can be (and is likely) negative.  This showed up on arm7, which apparently uses unsigned char for char.

--- sbr/m_getfld.c
+++ sbr/m_getfld.c
@@ -799,7 +799,7 @@ m_unknown(m_getfld_state_t *gstate, FILE *iob)
     s->msg_style = MS_UNKNOWN;
 
     for (i = 0, cp = text; i < sizeof text; ++i, ++cp) {
-	if ((*cp = Getc (s)) == EOF) {
+	if ((signed char) (*cp = Getc (s)) == EOF) {
 	    break;
 	}
     }
@@ -879,7 +879,7 @@ m_Eom (m_getfld_state_t s, int c)
     char *cp;
 
     for (i = 0, cp = text; i < s->edelimlen; ++i, ++cp) {
-	if ((*cp = Getc (s)) == EOF) {
+	if ((signed char) (*cp = Getc (s)) == EOF) {
 	    break;
 	}
     }
