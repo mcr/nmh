8e7da2908d70b96ee25587a6b6eb28cb84fb8459
David Levine <levinedl@acm.org>
2014-05-29 21:05:36 -0500

    Undid commit 475fef2edee9db3c63fc089df76f8e93e3d598cd, it didn't seem popular.  We'd be better off with a proper method, or using formail(1), to extract the addresses from a message.

--- uip/replsbr.c
+++ uip/replsbr.c
@@ -400,8 +400,6 @@ insert (struct mailname *np)
 	return 0;
 
     if (querysw) {
-        /* Don't change the "Reply to %s? " prompt format, so that
-           scripts can rely on it. */
 	snprintf (buffer, sizeof(buffer), "Reply to %s? ", adrformat (np));
 	if (!gans (buffer, anoyes))
 	return 0;
