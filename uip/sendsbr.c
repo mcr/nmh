7c545bcaf9158021853d3dc097d2e41b7417ad10
David Levine <levinedl@acm.org>
2016-07-05 12:02:59 -0400

    Moved sendfrom code from contrib into send(1).

--- uip/sendsbr.c
+++ uip/sendsbr.c
@@ -8,6 +8,8 @@
  */
 
 #include <h/mh.h>
+#include <h/fmt_scan.h>
+#include <h/fmt_compile.h>
 #include <h/signals.h>
 #include <setjmp.h>
 #include <fcntl.h>
@@ -20,6 +22,12 @@
 # include <sys/time.h>
 #endif
 #include <time.h>
+
+#ifdef OAUTH_SUPPORT
+#include <h/oauth.h>
+
+static int setup_oauth_params(char *[], int *, int, const char **);
+#endif /* OAUTH_SUPPORT */
 
 int debugsw = 0;		/* global */
 int forwsw  = 1;
@@ -38,14 +46,17 @@ static jmp_buf env;
 /*
  * static prototypes
  */
-static void armed_done (int) NORETURN;
 static void alert (char *, int);
 static int tmp_fd (void);
 static void anno (int, struct stat *);
 static void annoaux (int);
 static int splitmsg (char **, int, char *, char *, struct stat *, int);
 static int sendaux (char **, int, char *, char *, struct stat *);
-
+static void handle_sendfrom(char **, int *, char *);
+static int get_from_header_info(const char *, const char **, const char **, const char **);
+static const char *get_message_header_info(FILE *, char *);
+static void merge_profile_entry(const char *, const char *, char *[], int *);
+static void armed_done (int) NORETURN;
 
 /*
  * Entry point into (back-end) routines to send message.
@@ -61,6 +72,10 @@ sendsbr (char **vec, int vecp, char *program, char *draft, struct stat *st,
     struct stat sts;
     char **buildvec, *buildprogram;
     char *volatile drft = draft;
+    /* nvecs is volatile to prevent warning from gcc about possible clobbering
+       by longjmp. */
+    volatile int nvecs = vecp;
+    int *nvecsp = (int *) &nvecs;
 
     /*
      * Run the mimebuildproc (which is by default mhbuild) on the message
@@ -93,7 +108,7 @@ sendsbr (char **vec, int vecp, char *program, char *draft, struct stat *st,
 
     done=armed_done;
     switch (setjmp (env)) {
-    case OK: 
+    case OK:
 	/*
 	 * If given -push and -unique (which is undocumented), then
 	 * rename the draft file.  I'm not quite sure why.
@@ -109,16 +124,24 @@ sendsbr (char **vec, int vecp, char *program, char *draft, struct stat *st,
 	    drft = file;
 	}
 
+        /*
+         * Rework the vec based on From: header in draft, as specified
+         * by sendfrom-address entries in profile.
+         */
+        if (context_find_prefix("sendfrom-")) {
+            handle_sendfrom(vec, nvecsp, draft);
+        }
+
 	/*
 	 * Check if we need to split the message into
 	 * multiple messages of type "message/partial".
 	 */
 	if (splitsw >= 0 && !distfile && stat ((char *) drft, &sts) != NOTOK
 		&& sts.st_size >= CPERMSG) {
-	    status = splitmsg (vec, vecp, program, drft,
+	    status = splitmsg (vec, nvecs, program, drft,
 	    		       st, splitsw) ? NOTOK : OK;
 	} else {
-	    status = sendaux (vec, vecp, program, drft, st) ? NOTOK : OK;
+	    status = sendaux (vec, nvecs, program, drft, st) ? NOTOK : OK;
 	}
 
 	/* rename the original draft */
@@ -128,7 +151,7 @@ sendsbr (char **vec, int vecp, char *program, char *draft, struct stat *st,
 	    advise (buffer, "unable to rename %s to", drft);
 	break;
 
-    default: 
+    default:
 	status = DONE;
 	break;
     }
@@ -323,7 +346,7 @@ splitmsg (char **vec, int vecp, char *program, char *drft,
 		    break;
 		adios (NULL, "premature eof");
 	    }
-	    
+
 	    if ((pos += (len = strlen (buffer))) > CPERMSG) {
 		fseek (in, -len, SEEK_CUR);
 		break;
@@ -585,7 +608,7 @@ anno (int fd, struct stat *st)
 	    if (cwd == NULL)
 		cwd = getcpy (pwd ());
 
-	case OK: 
+	case OK:
 	    /* block a few signals */
 	    sigemptyset (&set);
 	    sigaddset (&set, SIGHUP);
@@ -699,6 +722,247 @@ annoaux (int fd)
 
 oops:
     folder_free (mp);	/* free folder/message structure */
+}
+
+
+static
+void
+handle_sendfrom(char **vec, int *vecp, char *draft) {
+    const char *addr, *host;
+    const char *message;
+
+    /* Extract address and host from From: header line in draft. */
+    if (get_from_header_info(draft, &addr, &host, &message) != OK) {
+        adios(draft, message);
+    }
+
+    /* Merge in any address or host specific switches to post(1) from profile. */
+    merge_profile_entry(addr, host, vec, vecp);
+    free((void *) host);
+    free((void *) addr);
+
+    vec[*vecp] = NULL;
+
+    {
+        char **vp;
+
+        for (vp = vec; *vp; ++vp) {
+            if (strcmp(*vp, "xoauth2") == 0) {
+#ifdef OAUTH_SUPPORT
+                int snoop = 0;
+
+                /* -snoop will be in vec if it was enabled. */
+                for (vp = vec; vp && *vp; ++vp) {
+                    if (strcmp(*vp, "-snoop") == 0) {
+                        snoop = 1;
+                        break;
+                    }
+                }
+
+                if (setup_oauth_params(vec, vecp, snoop, &message) != OK) {
+                    adios(NULL, message);
+                }
+                break;
+#else
+                adios(NULL, "sendfrom built without OAUTH_SUPPORT, "
+                      "so -saslmech xoauth2 is not supported");
+#endif /* OAUTH_SUPPORT */
+            }
+        }
+    }
+}
+
+
+#ifdef OAUTH_SUPPORT
+/*
+ * For XOAUTH2, append access token, from mh_oauth_do_xoauth(), for the user to vec.
+ */
+static
+int
+setup_oauth_params(char *vec[], int *vecp, int snoop, const char **message) {
+    const char *saslmech = NULL, *user = NULL, *auth_svc = NULL;
+    int i;
+
+    /* Make sure we have all the information we need. */
+    for (i = 1; i < *vecp; ++i) {
+        /* Don't support abbreviated switches, to avoid collisions in the
+           future if new ones are added. */
+        if (! strcmp(vec[i-1], "-saslmech")) {
+            saslmech = vec[i];
+        } else if (! strcmp(vec[i-1], "-user")) {
+            user = vec[i];
+        } else if (! strcmp(vec[i-1], "-authservice")) {
+            auth_svc = vec[i];
+        }
+    }
+
+    if (auth_svc == NULL) {
+        if (saslmech  &&  ! strcasecmp(saslmech, "xoauth2")) {
+            *message = "must specify -authservice with -saslmech xoauth2";
+            return NOTOK;
+        }
+    } else {
+        if (user == NULL) {
+            *message = "must specify -user with -saslmech xoauth2";
+            return NOTOK;
+        }
+
+        vec[(*vecp)++] = getcpy("-authservice");
+        if (saslmech  &&  ! strcasecmp(saslmech, "xoauth2")) {
+            vec[(*vecp)++] = mh_oauth_do_xoauth(user, auth_svc, snoop ? stderr : NULL);
+        } else {
+            vec[(*vecp)++] = getcpy(auth_svc);
+        }
+    }
+
+    return 0;
+}
+#endif /* OAUTH_SUPPORT */
+
+
+/*
+ * Extract user and domain from From: header line in draft.
+ */
+static
+int
+get_from_header_info(const char *filename, const char **addr, const char **host, const char **message) {
+    struct stat st;
+    FILE *in;
+
+    if (stat (filename, &st) == NOTOK) {
+        *message = "unable to stat draft file";
+        return NOTOK;
+    }
+
+    if ((in = fopen(filename, "r")) != NULL) {
+        char *addrformat = "%(addr{from})", *hostformat = "%(host{from})";
+
+        if ((*addr = get_message_header_info(in, addrformat)) == NULL) {
+            *message = "unable to find From: address in";
+            return NOTOK;
+        }
+        rewind(in);
+
+        if ((*host = get_message_header_info(in, hostformat)) == NULL) {
+            fclose(in);
+            *message = "unable to find From: host in";
+            return NOTOK;
+        }
+        fclose(in);
+
+        return OK;
+    } else {
+        *message = "unable to open";
+        return NOTOK;
+    }
+}
+
+
+/*
+ * Get formatted information from header of a message.
+ * Adapted from process_single_file() in uip/fmttest.c.
+ */
+static
+const char *
+get_message_header_info(FILE *in, char *format) {
+    int dat[5];
+    struct format *fmt;
+    struct stat st;
+    int parsing_header;
+    m_getfld_state_t gstate = 0;
+    charstring_t buffer = charstring_create(0);
+    char *retval;
+
+    dat[0] = dat[1] = dat[4] = 0;
+    dat[2] = fstat(fileno(in), &st) == 0  ?  st.st_size  :  0;
+    dat[3] = INT_MAX;
+
+    (void) fmt_compile(new_fs(NULL, NULL, format), &fmt, 1);
+    free_fs();
+
+    /*
+     * Read in the message and process the header.
+     */
+    parsing_header = 1;
+    do {
+        char name[NAMESZ], rbuf[NMH_BUFSIZ];
+        int bufsz = sizeof rbuf;
+        int state = m_getfld(&gstate, name, rbuf, &bufsz, in);
+
+        switch (state) {
+        case FLD:
+        case FLDPLUS: {
+            int bucket = fmt_addcomptext(name, rbuf);
+
+            if (bucket != -1) {
+                while (state == FLDPLUS) {
+                    bufsz = sizeof rbuf;
+                    state = m_getfld(&gstate, name, rbuf, &bufsz, in);
+                    fmt_appendcomp(bucket, name, rbuf);
+                }
+            }
+
+            while (state == FLDPLUS) {
+                bufsz = sizeof rbuf;
+                state = m_getfld(&gstate, name, rbuf, &bufsz, in);
+            }
+            break;
+        }
+        default:
+            parsing_header = 0;
+        }
+    } while (parsing_header);
+    m_getfld_state_destroy(&gstate);
+
+    fmt_scan(fmt, buffer, INT_MAX, dat, NULL);
+    fmt_free(fmt, 1);
+
+    /* Trim trailing newline, if any. */
+    retval = rtrim(charstring_buffer_copy((buffer)));
+    charstring_free(buffer);
+
+    return retval;
+}
+
+
+/*
+ * Look in profile for entry corresponding to addr or host, and add its contents to vec.
+ *
+ * Could do some of this automatically, by looking for:
+ * 1) access-$(mbox{from}) in oauth-svc file using mh_oauth_cred_load(), which isn't
+ *    static and doesn't have side effects; free the result with mh_oauth_cred_free())
+ * 2) machine $(mbox{from}) in creds
+ * If no -server passed in from profile or commandline, could use smtp.<svc>.com for gmail,
+ * but that might not generalize for other svcs.
+ */
+static
+void
+merge_profile_entry(const char *addr, const char *host, char *vec[], int *vecp) {
+    char *addr_entry = concat("sendfrom-", addr, NULL);
+    char *profile_entry = context_find(addr_entry);
+
+    free(addr_entry);
+    if (profile_entry == NULL) {
+        /* No entry for the user.  Look for one for the host. */
+        char *host_entry = concat("sendfrom-", host, NULL);
+
+        profile_entry = context_find(host_entry);
+        free(host_entry);
+    }
+
+    /* Use argsplit() to do the real work of splitting the args in the profile entry. */
+    if (profile_entry  &&  strlen(profile_entry) > 0) {
+        int profile_vecp;
+        char *file;
+        char **profile_vec = argsplit(profile_entry, &file, &profile_vecp);
+        int i;
+
+        for (i = 0; i < profile_vecp; ++i) {
+            vec[(*vecp)++] = getcpy(profile_vec[i]);
+        }
+
+        arglist_free(file, profile_vec);
+    }
 }
 
 
