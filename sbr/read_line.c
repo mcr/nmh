c3eea81ded78e7803b42f09697bc202e1b1aaf86
Ralph Corderoy <ralph@inputplus.co.uk>
2016-10-19 00:50:28 +0100

    Replace strchr() condition with TrimSuffixC().

--- sbr/read_line.c
+++ sbr/read_line.c
@@ -1,16 +1,15 @@
 #include <h/mh.h>
+#include <h/utils.h>
 
 const char *
 read_line(void)
 {
-    char *cp;
     static char line[BUFSIZ];
 
     fflush(stdout);
     if (fgets(line, sizeof(line), stdin) == NULL)
             return NULL;
-    if ((cp = strchr(line, '\n')))
-	*cp = 0;
-    return line;
-}
+    TrimSuffixC(line, '\n');
 
+    return line; /* May not be a complete line. */
+}
