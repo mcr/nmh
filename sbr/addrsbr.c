19e4b527a4256490d4d143465e86dd205d618b2e
Ralph Corderoy <ralph@inputplus.co.uk>
2016-10-22 00:18:26 +0100

    Replace `if (p) free(p)' with `mh_xfree(p)'.

--- sbr/addrsbr.c
+++ sbr/addrsbr.c
@@ -206,20 +206,13 @@ mnfree (struct mailname *mp)
     if (!mp)
 	return;
 
-    if (mp->m_text)
-	free (mp->m_text);
-    if (mp->m_pers)
-	free (mp->m_pers);
-    if (mp->m_mbox)
-	free (mp->m_mbox);
-    if (mp->m_host)
-	free (mp->m_host);
-    if (mp->m_path)
-	free (mp->m_path);
-    if (mp->m_gname)
-	free (mp->m_gname);
-    if (mp->m_note)
-	free (mp->m_note);
+    mh_xfree(mp->m_text);
+    mh_xfree(mp->m_pers);
+    mh_xfree(mp->m_mbox);
+    mh_xfree(mp->m_host);
+    mh_xfree(mp->m_path);
+    mh_xfree(mp->m_gname);
+    mh_xfree(mp->m_note);
 
     free ((char *) mp);
 }
