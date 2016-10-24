ef7141856b222b70b83c8e64c37b99cc0f6475a9
Ralph Corderoy <ralph@inputplus.co.uk>
2016-10-24 23:36:52 +0100

    Replace putc() with putchar().
    Others already existed.  Not being a macro probably won't hurt.

--- uip/fmtdump.c
+++ uip/fmtdump.c
@@ -492,10 +492,10 @@ static void
 litputs(char *s)
 {
 	if (s) {
-		putc('"', stdout);
+		putchar('"');
 		while (*s)
 			litputc(*s++);
-		putc('"', stdout);
+		putchar('"');
 	} else
 		fputs("<nil>", stdout);
 }
@@ -504,30 +504,30 @@ static void
 litputc(char c)
 {
 	if (c & ~ 0177) {
-		putc('M', stdout);
-		putc('-', stdout);
+		putchar('M');
+		putchar('-');
 		c &= 0177;
 	}
 	if (c < 0x20 || c == 0177) {
 		if (c == '\b') {
-			putc('\\', stdout);
-			putc('b', stdout);
+			putchar('\\');
+			putchar('b');
 		} else if (c == '\f') {
-			putc('\\', stdout);
-			putc('f', stdout);
+			putchar('\\');
+			putchar('f');
 		} else if (c == '\n') {
-			putc('\\', stdout);
-			putc('n', stdout);
+			putchar('\\');
+			putchar('n');
 		} else if (c == '\r') {
-			putc('\\', stdout);
-			putc('r', stdout);
+			putchar('\\');
+			putchar('r');
 		} else if (c == '\t') {
-			putc('\\', stdout);
-			putc('t', stdout);
+			putchar('\\');
+			putchar('t');
 		} else {
-			putc('^', stdout);
-			putc(c ^ 0x40, stdout);	/* DEL to ?, others to alpha */
+			putchar('^');
+			putchar(c ^ 0x40);	/* DEL to ?, others to alpha */
 		}
 	} else
-		putc(c, stdout);
+		putchar(c);
 }
