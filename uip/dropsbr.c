mcr/nmh

commit 28610ff9a604a75ae0c383be03aa19415ddb1965

Null-terminate buffer in mbx_copy() in dropsbr.c.  Noticed by valgrind.

uip/dropsbr.c
@@ -306,6 +306,10 @@ mbx_copy (char *mailbox, int mbx_style, int md, int fd,
 	    }
 		    
 	    while ((i = read (fd, buffer, sizeof(buffer))) > 0) {
+                /* valgrind noticed that stringdex depends on null
+                   termination. */
+                buffer[i] = '\0';
+
 		for (j = 0;
 			(j = stringdex (mmdlm1, buffer)) >= 0;
 			buffer[j]++)

commit ab7f4c906111c206b91236ed8c210e681c85f338

uip/dropsbr.c: Fix buffer overrun in mbx_copy().
The overrun occurs frequently, but typically zeroes a byte of a
character pointer on the stack that's not yet been used.
Caused by 28610ff9a604a75ae0c383be03aa19415ddb1965 appending a NUL after
the bytes stored by read(2).

uip/dropsbr.c
@@ -272,7 +272,7 @@ mbx_copy (char *mailbox, int mbx_style, int md, int fd,
     int i, j, size;
     off_t start, stop;
     long pos;
-    char *cp, buffer[BUFSIZ];
+    char *cp, buffer[BUFSIZ + 1];   /* Space for NUL. */
     FILE *fp;
 
     pos = (long) lseek (md, (off_t) 0, SEEK_CUR);
@@ -295,10 +295,8 @@ mbx_copy (char *mailbox, int mbx_style, int md, int fd,
 			size++;
 	    }
 		    
-	    while ((i = read (fd, buffer, sizeof(buffer))) > 0) {
-                /* valgrind noticed that stringdex depends on null
-                   termination. */
-                buffer[i] = '\0';
+	    while ((i = read (fd, buffer, sizeof buffer - 1)) > 0) {
+                buffer[i] = '\0';   /* Terminate for stringdex(). */
 
 		for ( ;	(j = stringdex (mmdlm1, buffer)) >= 0; buffer[j]++)
 		    continue;
@@ -351,7 +349,7 @@ mbx_copy (char *mailbox, int mbx_style, int md, int fd,
 		     * back to "From ".
 		     */
                     if (has_prefix(buffer, "Return-Path:")) {
-			char tmpbuffer[BUFSIZ];
+			char tmpbuffer[sizeof buffer];
 			char *tp, *ep, *fp;
 
 			strncpy(tmpbuffer, buffer, sizeof(tmpbuffer));
@@ -366,7 +364,7 @@ mbx_copy (char *mailbox, int mbx_style, int md, int fd,
 			 * Change the "X-Envelope-From:" field
 			 * (if first line) back to "From ".
 			 */
-			char tmpbuffer[BUFSIZ];
+			char tmpbuffer[sizeof buffer];
 			char *ep;
 
 			strncpy(tmpbuffer, buffer, sizeof(tmpbuffer));
@@ -377,7 +375,7 @@ mbx_copy (char *mailbox, int mbx_style, int md, int fd,
 			 * If there is already a "From " line,
 			 * then leave it alone.  Else we add one.
 			 */
-			char tmpbuffer[BUFSIZ];
+			char tmpbuffer[sizeof buffer];
 			char *tp, *ep;
 
 			strncpy(tmpbuffer, buffer, sizeof(tmpbuffer));