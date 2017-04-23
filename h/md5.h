0c3a21f9763759559bc064112dda39f9a4561db2
Ralph Corderoy <ralph@inputplus.co.uk>
2017-04-23 14:36:47 +0100

    md5.h: Remove unused UINT2 typedef for unsigned short.

--- h/md5.h
+++ h/md5.h
@@ -36,9 +36,6 @@
 /* POINTER defines a generic pointer type */
 typedef unsigned char *POINTER;
 
-/* UINT2 defines a two byte word */
-typedef unsigned short int UINT2;
-
 /* UINT4 defines a four byte word */
 typedef uint32_t UINT4;
 
