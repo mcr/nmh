571d5e7543fd73aeb77af653a8d1b20d78c84b48
Ralph Corderoy <ralph@inputplus.co.uk>
2017-05-09 23:10:06 +0100

    config/version.sh: Rewrite.  Use uname(1), git-describe(1), and UTC.
    Not sure why it searched through PATH manually for uname(1) and
    hostname(1).  uname and its -n option are POSIX so just use those.  It
    was the preference over hostname anyway.
    
    Use git-describe(1), not just git-branch(1), as it gives more detail,
    including --dirty to show the built source differs from the commit.
    
    Specify the format for the build date, and its timezone; +0000.

--- config/version.sh
+++ config/version.sh
@@ -1,50 +1,21 @@
-#!/bin/sh
+#! /bin/sh
 #
 # version.sh -- script to create version string(s) for nmh.
-#
 # You need to pass the script the version number to use.
-#
 
-if [ -z "$1" ]; then
-    echo "usage: version.sh VERSION" 1>&2
-    exit 1
-fi
+set -e
 
-VERSION=$1
-OFS="$IFS"
-IFS=:
-HOSTNAME=unknown
-
-# Find out the name of the host we are compiling on
-for prog in uname hostname
-do
-    for dir in $PATH
-    do
-	if [ ! -f $dir/$prog ]; then
-	    continue
-	fi
-	case $prog in
-	    uname)	HOSTNAME=`$prog -n`
-			;;
-	    hostname)	HOSTNAME=`$prog`
-			;;
-	esac
-	break
-    done
-    if [ X"$HOSTNAME" != X  -a  X"$HOSTNAME" != Xunknown ]; then
-	break
-    fi
-done
-
-IFS=" "
-
-if [ -d "${srcdir}/.git" ] ; then
-    branch=`(git branch | grep '^\*' | tr -d '* ') || true`
-fi
-if [ "${branch}" -a "${branch}" != "master" ] ; then
-    echo "char *version_str = \"nmh-$VERSION [branch ${branch}] [compiled on $HOSTNAME at `date`]\";"
+version=${1?}
+host=`uname -n`
+if test -d "$srcdir/.git"; then
+    git=" `git describe --long --dirty`"
 else
-    echo "char *version_str = \"nmh-$VERSION [compiled on $HOSTNAME at `date`]\";"
+    git=
 fi
-echo "char *version_num = \"nmh-$VERSION\";"
-echo "char *user_agent = \"nmh/$VERSION\";"
+date="`TZ=GMT0 date +'%Y-%m-%d %T'` +0000"
+
+cat <<E
+char *version_str = "nmh-$version$git built $date on $host";
+char *version_num = "nmh-$version";
+char *user_agent = "nmh/$version";
+E
