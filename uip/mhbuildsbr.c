9a603b2b55d3d25e6201a56d5d2431fce17970d5
Ralph Corderoy <ralph@inputplus.co.uk>
2017-09-08 14:30:31 +0100

    mhbuildsbr.c: Attempt fork(2) multiple times, as intended.
    The loop attempting fork() intends to try five times but the comparison
    operator was wrong so would only try once.  Bug present since pre-git.

--- uip/mhbuildsbr.c
+++ uip/mhbuildsbr.c
@@ -1321,7 +1321,7 @@ raw:
 	    if ((out = fopen (ce->ce_file, "w")) == NULL)
 		adios (ce->ce_file, "unable to open for writing");
 
-	    for (i = 0; (child_id = fork()) == NOTOK && i > 5; i++)
+	    for (i = 0; (child_id = fork()) == NOTOK && i < 5; i++)
 		sleep (5);
 	    switch (child_id) {
 	    case NOTOK:
