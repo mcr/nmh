2b383bf8c8afa623bd84cc69284a632c71e78e94
Ralph Corderoy <ralph@inputplus.co.uk>
2017-11-02 01:46:26 +0000

    md5.c, md5.h: Reformat and delete needless portability.
    The algorithm is not going to change, bugs are unlikely to appear in the
    upstream RSA version, so reformat the source to be more readable, and
    delete portability code now we have function prototypes with parameters,
    void pointer, etc.

--- h/md5.h
+++ h/md5.h
@@ -6,48 +6,11 @@
  * RSAREF types and constants
  */
 
-/*
- * Use include for nmh/mh
- */
-
 #include <h/nmh.h>
-
-/*
- * We need this for uint32_t
- */
-
 #include <inttypes.h>
-
-/*
- * Use prototypes for nmh/mh
- */
-#define PROTOTYPES 1
-
-/*
- * PROTOTYPES should be set to one if and only if the compiler
- * supports function argument prototyping.  The following makes
- * PROTOTYPES default to 0 if it has not already been defined
- * with C compiler flags.
- */
-#ifndef PROTOTYPES
-#define PROTOTYPES 0
-#endif
-
-/* POINTER defines a generic pointer type */
-typedef unsigned char *POINTER;
 
 /* UINT4 defines a four byte word */
 typedef uint32_t UINT4;
-
-/* PROTO_LIST is defined depending on how PROTOTYPES is defined above.
-If using PROTOTYPES, then PROTO_LIST returns the list, otherwise it
-  returns an empty list.
- */
-#if PROTOTYPES
-#define PROTO_LIST(list) list
-#else
-#define PROTO_LIST(list) ()
-#endif
 
 /* MD5.H - header file for MD5C.C
  */
@@ -76,11 +39,11 @@ documentation and/or software.
 
 /* MD5 context. */
 typedef struct {
-  UINT4 state[4];                                   /* state (ABCD) */
-  UINT4 count[2];        /* number of bits, modulo 2^64 (lsb first) */
-  unsigned char buffer[64];                         /* input buffer */
+    UINT4 state[4]; /* state (ABCD) */
+    UINT4 count[2]; /* number of bits, modulo 2^64 (lsb first) */
+    unsigned char buffer[64]; /* input buffer */
 } MD5_CTX;
 
-void MD5Init PROTO_LIST ((MD5_CTX *));
-void MD5Update PROTO_LIST ((MD5_CTX *, unsigned char *, unsigned int));
-void MD5Final PROTO_LIST ((unsigned char [16], MD5_CTX *));
+void MD5Init(MD5_CTX *);
+void MD5Update(MD5_CTX *, unsigned char *, unsigned int);
+void MD5Final(unsigned char [16], MD5_CTX *);
