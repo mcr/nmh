1d3b1bec14ae6bd849053c9746e7da552bf539fa
Ralph Corderoy <ralph@inputplus.co.uk>
2017-11-02 22:37:16 +0000

    seq_bits.c: Move interface to own file.

--- sbr/seq_bits.c
+++ sbr/seq_bits.c
@@ -6,6 +6,7 @@
  */
 
 #include "h/mh.h"
+#include "seq_bits.h"
 
 
 char *
---
+++ sbr/seq_bits.h
@@ -0,0 +1,7 @@
+/* seq_bits.h -- return the snprintb() string for a sequence
+ *
+ * This code is Copyright (c) 2017, by the authors of nmh.  See the
+ * COPYRIGHT file in the root directory of the nmh distribution for
+ * complete copyright information. */
+
+char *seq_bits(struct msgs *);
