0b05f0175b632c856ea4a2c4ae10c84694a1f385
Ralph Corderoy <ralph@inputplus.co.uk>
2017-11-07 00:10:06 +0000

    atooi.c: Move interface to own file.

---
+++ sbr/atooi.h
@@ -0,0 +1,7 @@
+/* atooi.h -- octal version of atoi()
+ *
+ * This code is Copyright (c) 2017, by the authors of nmh.  See the
+ * COPYRIGHT file in the root directory of the nmh distribution for
+ * complete copyright information. */
+
+int atooi(char *) NONNULL(1) PURE;
--- sbr/m_gmprot.c
+++ sbr/m_gmprot.c
@@ -6,6 +6,7 @@
  */
 
 #include "h/mh.h"
+#include "atooi.h"
 
 
 int
