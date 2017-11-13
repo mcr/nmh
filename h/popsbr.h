a3970d298495e0b6358541b2c8dd0ce6c32fe43c
Ralph Corderoy <ralph@inputplus.co.uk>
2017-11-13 00:06:30 +0000

    popsbr.h: Remove pop_fd() and pop_set() prototypes.
    Function definitions removed in 4ea2f92f.

--- h/popsbr.h
+++ h/popsbr.h
@@ -6,10 +6,8 @@
 #define P_NOVERIFY 0x02
 int pop_init (char *, char *, char *, char *, int, int, char *, int,
 	      const char *);
-int pop_fd (char *, int, char *, int);
 int pop_stat (int *, int *);
 int pop_retr (int, int (*)(void *, char *), void *);
 int pop_dele (int);
 int pop_quit (void);
 int pop_done (void);
-int pop_set (int, int, int);
