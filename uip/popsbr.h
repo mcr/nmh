79ed2204e93a3ace4cdd76f0628168402582e3bd
Ralph Corderoy <ralph@inputplus.co.uk>
2017-11-14 23:41:18 +0000

    popsbr.h: Base header on implementation.
    Normally done as part of extracting the new header file from a larger
    one, but this file already existed so it had to be moved as one commit
    and altered in the next, otherwise git would break its history.

--- uip/popsbr.h
+++ uip/popsbr.h
@@ -1,13 +1,16 @@
-/* popsbr.h -- header for POP client subroutines
- */
+/* popsbr.h -- POP client subroutines
+ *
+ * This code is Copyright (c) 2017, by the authors of nmh.  See the
+ * COPYRIGHT file in the root directory of the nmh distribution for
+ * complete copyright information. */
 
 /* TLS flags */
 #define P_INITTLS 0x01
 #define P_NOVERIFY 0x02
-int pop_init (char *, char *, char *, char *, int, int, char *, int,
-	      const char *);
-int pop_stat (int *, int *);
-int pop_retr (int, int (*)(void *, char *), void *);
-int pop_dele (int);
-int pop_quit (void);
-int pop_done (void);
+
+int pop_init(char *, char *, char *, char *, int, int, char *, int, const char *);
+int pop_stat(int *, int *);
+int pop_retr(int, int (*)(void *, char *), void *);
+int pop_dele(int);
+int pop_quit(void);
+int pop_done(void);
