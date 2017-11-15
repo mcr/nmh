3c54bbb247ffc8fa094d754099c0b009e79d484d
Ralph Corderoy <ralph@inputplus.co.uk>
2017-11-15 23:29:04 +0000

    crawl_folders.h: Base header on implementation.
    Normally done as part of extracting the new header file from a larger
    one, but this file already existed so it had to be moved as one commit
    and altered in the next, otherwise git would break its history.

--- sbr/crawl_folders.h
+++ sbr/crawl_folders.h
@@ -1,14 +1,17 @@
 /* crawl_folders.h -- crawl folder hierarchy
- */
+ *
+ * This code is Copyright (c) 2017, by the authors of nmh.  See the
+ * COPYRIGHT file in the root directory of the nmh distribution for
+ * complete copyright information. */
 
 #define CRAWL_NUMFOLDERS 100
 
 /* Callbacks return true crawl_folders should crawl the children of `folder'.
  * Callbacks need not duplicate folder, as crawl_folders does not free it. */
-typedef bool (crawl_callback_t)(char *folder, void *baton);
+typedef bool (crawl_callback_t)(char *, void *);
 
 /* Crawl the folder hierarchy rooted at the relative path `dir'.  For each
  * folder, pass `callback' the folder name (as a path relative to the current
  * directory) and `baton'; the callback may direct crawl_folders not to crawl
  * its children; see above. */
-void crawl_folders (char *dir, crawl_callback_t *callback, void *baton);
+void crawl_folders(char *, crawl_callback_t *, void *);
